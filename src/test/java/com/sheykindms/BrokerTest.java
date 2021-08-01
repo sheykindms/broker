a9fa062b9298ce292d4b4c94668643e8c90fc2b0
Sheykin <sheykindms>
2021-08-01 14:29:04 +0000

    Initial commit

---
+++ src/test/java/com/sheykindms/BrokerTest.java
@@ -0,0 +1,21 @@
+package com.sheykindms;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class BrokerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
